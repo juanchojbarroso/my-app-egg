4f42778611e61cb159d5db7e11a23eecf6388baf
juanchojer <juan.barroso.025@gmail.com>
2017-07-15 21:57:59 +0100

    Cambios en app.js
    creando y configurando "React.Component{}"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-const App = () => <h1>Hi</h1>
+//First way for represent code 
+class App extends React.Component {
+    render(){
+        return(
+        <div>
+            <h1>Hello World</h1>
+            <b>Bold</b>
+        </div>
+        ) 
+    }
+}
+
+//Second way for represent the same code
+/*class App extends React.Component {
+    render(){
+        return React.createElement('h1', null, 'Hello Egghead')
+    }
+}*/
+
+//other way for represent the same code
+
+//const App = () => <h1>Hello stateless jaja</h1>
+
 
 export default App
