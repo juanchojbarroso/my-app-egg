feb6d66f76e25c4b45cf6e48fe107d778532dcc5
juanchojer <juan.barroso.025@gmail.com>
2017-07-16 01:08:32 +0100

    Add Custom propType Validation
    Add Custom propType Validation to React Components

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 }
 */
 
+
 //Second way for represent the same code
 /*class App extends React.Component {
     render(){
@@ -46,10 +47,10 @@ App.defaultProps = {
 */
 
 
-
 //Manage React Component State with setState
 
-class App extends React.Component {
+//Add Custom propType Validation to React Components
+/*class App extends React.Component {
     constructor(){
         super();
         this.state = {
@@ -71,6 +72,27 @@ class App extends React.Component {
 }
 
 const Widget = (props) =>
-    <input type="text" onChange={props.update} />
+    <input type="text" onChange={props.update} />*/
+
+
+
+class App extends React.Component {
+    render(){
+        return <Title text="1234567" />
+    }
+}
+
+const Title = (props) => <h1>Title: {props.txt}</h1>
+
+Title.propTypes = {
+    text(props, propName, component){
+        if(!(propName in props)){
+            return new Error('missing $(propName)')
+        }
+        if(props[propName].length < 6){
+            return new Error('${propName} was too short')
+        }
+    }
+}
 
 export default App
